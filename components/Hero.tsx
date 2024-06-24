b67e47fdf4bc999b38d72965333e50746ab0ac83
Kadi M <kicktheball1@gmail.com>
2024-06-24 20:27:03 +0200

    Project setup

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Spotlight } from './ui/Spotlight'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='pb-20 pt-36'>
+        <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen' fill='gray'/>
+        <Spotlight className='top-10 left-full h-[80vh] w-[50vw]' fill='purple'/>
+        <Spotlight className='top-20 left-80 h-[80vh] w-[50vw]' fill='blue'/>
+hi
+    </div>
   )
 }
 
