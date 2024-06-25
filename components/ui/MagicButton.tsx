8271d5ff359de73e9af41c795df08f545d9c73d4
Kadi M <kicktheball1@gmail.com>
2024-06-25 20:09:32 +0200

    Setting up hero section

--- components/ui/MagicButton.tsx
+++ components/ui/MagicButton.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const MagicButton = ({title, icon, position, handleClick}: {title:string, icon:string, position:string, handleClick?: ()=>void}) => {
+const MagicButton = ({title, icon, position, handleClick}: {title:string, icon:any, position:string, handleClick?: ()=>void}) => {
   return (
     <button className="relative inline-flex h-12 overflow-hidden rounded-lg p-[1px] focus:outline-none md:w-60 md:mt-10">
       <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
