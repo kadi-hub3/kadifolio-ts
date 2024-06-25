067f089fdcd8b95dbb0d92ae7dd22967ab036af2
Kadi M <kicktheball1@gmail.com>
2024-06-25 19:10:17 +0200

    Hero Section Animation & data

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/Hero";
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative ">
+    <main className="relative bg-gray-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className='max-w-7xl w-full'>
       <Hero/>
+
+      </div>
     </main>
   );
 }
