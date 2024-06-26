cab66be9819f935eda07e2fe2ad39a615a146989
Kadi M <kicktheball1@gmail.com>
2024-06-26 19:47:41 +0200

    Projects section

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -11,7 +11,7 @@ const Projects = () => {
         <div className='flex flex-wrap items-center justify-center p-4 gap-10 mt-10'>
             {projects.map(({id, title, des, img, iconLists, link})=> (
                 <div key={id} className='lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm_w-96 w-[80vw]'>
-                    <PinContainer title={title} href={link}> 
+                    <PinContainer title={link} href={link}> 
                         <div className='relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh]'>
                             <div>
                                 <img src='/bg.png' alt='bg-img' />
