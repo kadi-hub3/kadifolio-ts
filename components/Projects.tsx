b7a5a8f0a6fadfee624da428c6640f7d956b4ac5
Kadi M <kicktheball1@gmail.com>
2024-06-26 19:51:40 +0200

    Projects section

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -3,7 +3,7 @@ import { projects } from '../data'
 import { PinContainer } from './ui/3DPin'
 const Projects = () => {
   return (
-    <div className='py-20'>
+    <div className='py-20' id='projects'>
         <h1 className='heading'>
             A small selection of {' '}
             <span className='text-purple'>recent projects</span>
