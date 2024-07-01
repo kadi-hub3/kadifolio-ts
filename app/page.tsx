fe7d8c4747f0688a64e88b29da4e5b74b59f4a26
Kadi M <kicktheball1@gmail.com>
2024-07-01 20:21:02 +0200

    Home Intro section

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Projects from "@/components/Projects";
 
 export default function Home() {
   return (
-    <main className="relative bg-gray-600 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-gray-600 flex justify-center items-center flex-col overflow-hidden mx-auto">
       <div className='max-w-7xl w-full'>
       <FloatingNav className='bg-gray-50' navItems={navItems} />
       <Hero/>
--- components/HomeIntro.tsx
+++ components/HomeIntro.tsx
@@ -3,16 +3,16 @@ import { TextGenerateEffect } from "./ui/TextGenerateEffect"
 
 const HomeIntro = () => {
   return (
-    <section className='w-screen h-70vh sm:h-90vh bg-gray-200'>
+    <section className='w-screen h-70vh sm:h-90vh bg-gray-200 p-20'>
         <div className=''>
-            <div className='flex flex-row md:flex-col sm:flex-col items-center justify-center'>
+            <div className='flex flex-row md:flex-col sm:flex-col items-center justify-center gap-6'>
                 <div>
                     <TextGenerateEffect
                         className='text-black text-start text-[30px] md:text-2xl lg:text-4xl'
                         words="Helping companies achieve their goals using cutting-edge technology, we redefine standards and translate client specifications into digital solutions using the latest tech stack or precise requirements. No nonsense, just innovation. Join us to be at the forefront of digital transformation."
                     />
                 </div>
-                <div>
+                <div className='flex flex-col gap-x-4'>
                     <TextGenerateEffect 
                             className='text-center text-[20px] md:text-xl lg:text-2xl'
                             words="Combining my passion for design, coding, and interactive experiences, I am uniquely positioned in the web design world to help you achieve your goals and advance your mission. /n"
