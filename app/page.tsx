4103624709a983afff4b7f43caad7b5b8bb5783b
Kadi M <kicktheball1@gmail.com>
2024-07-02 18:16:12 +0200

    footer

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,18 @@ import {navItems} from '../data'
 import HomeIntro from "@/components/HomeIntro";
 import Grid from "@/components/Grid";
 import Projects from "@/components/Projects";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <main className="relative bg-gray-600 flex justify-center items-center flex-col overflow-hidden mx-auto">
       <div className='max-w-7xl w-full'>
-      <FloatingNav className='bg-gray-50' navItems={navItems} />
-      <Hero/>
-      <HomeIntro />
-      <Grid />
-      <Projects />
+        <FloatingNav className='bg-gray-50' navItems={navItems} />
+        <Hero/>
+        <HomeIntro />
+        <Grid />
+        <Projects />
+        <Footer />
       </div>
     </main>
   );
